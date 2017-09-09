165189b3d1818b668fc2bc8dcd1a3fa3fc010dde
Ayoub <chahib.ayoub@gmail.com>
2017-09-09 18:48:09 +0200

    push

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyA55t-PuDKDcg4kosuXdCMGLOOSsGa-ZrQ",
+    authDomain: "ma-chat-box-6ff87.firebaseapp.com",
+    databaseURL: "https://ma-chat-box-6ff87.firebaseio.com"
+});
+
+export default base;
---
+++ src/components/Connexion.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Connexion extends React.Component{
+
+   goToChat = event => {
+       event.preventDefault();
+       //Recuperer le pseudo
+       const pseudo = this.pseudoInput.value;
+       //Changer Url
+        this.context.router.transitionTo(`/pseudo/${pseudo}`);
+   };
+
+
+
+    render(){
+        return(
+            <div className="connexionBox" onSubmit={ e => this.goToChat(e)} >
+                <form className="connexion">
+                    <input type="text" placeholder="Pseudo" required  ref={input => {
+                        this.pseudoInput = input}}
+                    />
+                    <button type="submit">Go</button>
+
+                </form>
+            </div>
+        )
+    }
+
+    static  contextTypes = {
+        router: React.PropTypes.object
+    }
+}
+
+
+export default Connexion;
